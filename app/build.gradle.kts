ad846548a13277c12ff28d89237c158f425aac8f
Evgeniy Kanovalov <jonnypovidlos@yandex.ru>
2025-03-31 14:59:24 +0500

    basic configure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    id("java")
+    application
+    checkstyle
+    jacoco
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "hexlet.code"
@@ -16,4 +20,24 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "JonnyPovidlos_java-project-78")
+        property("sonar.organization", "jonnypovidlos")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
